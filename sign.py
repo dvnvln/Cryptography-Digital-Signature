0b3e4382fac4cd5184dfa10f80cb8ed9b1d1ae6a
dimaslucky <13518003@std.stei.itb.ac.id>
2021-11-21 03:52:53 +0700

    init + SHA + RSA

---
+++ sign.py
@@ -0,0 +1,6 @@
+from RSA import *
+from SHA256 import *
+
+text = "Pada wisuda sarjana baru ITB, ternyata ada seorang wisudawan yang paling muda. Umurnya baru 19 tahum. Ini berarti dia masuk ITB pada umur 15 tahum. Zaman sekarang banyak sarjana masih berusia muda belia. Mungkin masuk sekolah pada usia dini dan mengikuti kelas akselerasi pada tingkatan SD, SMP, dan SMA. Masuk SD umur 6 tahun dan ikut kelas aksel sehingga selesai dalam waktu lima tahun pada umur 11. SMP diselesaikan dalam waktu dua tahun dan SMA dalam waktu dua tahun, sehingga lulus SMA pada umur 15 tahun. Kuliah di ITB selama empat tahun sehingga wajar saja menjadi sarjana pada umur 19 tahun."
+text2 = "hello world"
+print(sha256(text2))
