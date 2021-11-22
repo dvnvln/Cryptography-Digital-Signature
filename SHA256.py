539b76852b25421bc8ce4233acbfb1e15e075e55
dvnvln <devinvalentinus1st@gmail.com>
2021-11-22 23:03:57 +0700

    update sha

--- SHA256.py
+++ SHA256.py
@@ -63,27 +63,44 @@ def preprocessMessage(message):
         bits[-64:] = message_len
     return chunker(bits, 512)
 
-def isTrue(x): return x == 1
+def isTrue(x): 
+    return x == 1
 
-def if_(i, y, z): return y if isTrue(i) else z
+def if_(i, y, z):
+    if isTrue(i):
+        return y  
+    else:
+        return z
 
-def and_(i, j): return if_(i, j, 0)
-def AND(i, j): return [and_(ia, ja) for ia, ja in zip(i,j)] 
+def and_(i, j): 
+    return (if_(i, j, 0))
 
-def not_(i): return if_(i, 0, 1)
-def NOT(i): return [not_(x) for x in i]
+def AND(i, j): 
+    return [and_(ia, ja) for ia, ja in zip(i,j)] 
 
-def xor(i, j): return if_(i, not_(j), j)
-def XOR(i, j): return [xor(ia, ja) for ia, ja in zip(i, j)]
+def not_(i): 
+    return (if_(i, 0, 1))
+def NOT(i): 
+    return [not_(x) for x in i]
 
-def xorxor(i, j, l): return xor(i, xor(j, l))
-def XORXOR(i, j, l): return [xorxor(ia, ja, la) for ia, ja, la, in zip(i, j, l)]
+def xor(i, j): 
+    return (if_(i, not_(j), j))
+def XOR(i, j): 
+    return ([xor(ia, ja) for ia, ja in zip(i, j)])
 
-def maj(i,j,k): return max([i,j,], key=[i,j,k].count)
+def xorxor(i, j, l): 
+    return (xor(i, xor(j, l)))
+def XORXOR(i, j, l): 
+    return ([xorxor(ia, ja, la) for ia, ja, la, in zip(i, j, l)])
 
-def rotr(x, n): return x[-n:] + x[:-n]
+def maj(i,j,k): 
+    return (max([i,j,], key=[i,j,k].count))
 
-def shr(x, n): return n * [0] + x[:-n]
+def rotr(x, n): 
+    return (x[-n:] + x[:-n])
+
+def shr(x, n): 
+    return (n * [0] + x[:-n])
 
 def add(i, j):
     length = len(i)
